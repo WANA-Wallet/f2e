8033aaad8e39d07ecccb2b7c737724c4d5e210be
gjlmotea <gjlmotea@gmail.com>
2022-03-27 13:35:06 +0800

    電腦版換裝 done

--- dress.js
+++ dress.js
@@ -5,7 +5,7 @@ let swiperDressEyesList = new Swiper(".swiper.dress-eyes", {
     spaceBetween: 50,
     autoHeight: true,
     resizeObserver: true,
-    speed: 500,
+    speed: 200,
     allowSlideNext: true,
     allowSlidePrev: true,
     centeredSlides: false,
@@ -31,12 +31,12 @@ let swiperDressEyesList = new Swiper(".swiper.dress-eyes", {
 //     });
 // })
 
-let swiperDressClothes = new Swiper(".swiper.dress-clothes", {
+let swiperDressClothesList = new Swiper(".swiper.dress-clothes", {
     slidesPerView: 1,
     spaceBetween: 50,
     autoHeight: true,
     resizeObserver: true,
-    speed: 500,
+    speed: 200,
     allowSlideNext: true,
     allowSlidePrev: true,
     centeredSlides: false,
@@ -49,6 +49,52 @@ let swiperDressClothes = new Swiper(".swiper.dress-clothes", {
     navigation: {
         nextEl: '.swiper-button-next.dress-clothes',
         prevEl: '.swiper-button-prev.dress-clothes',
+    },
+    allowTouchMove: true,
+    preventClicks: true,
+});
+
+let swiperDressHairList = new Swiper(".swiper.dress-hair", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    autoHeight: true,
+    resizeObserver: true,
+    speed: 200,
+    allowSlideNext: true,
+    allowSlidePrev: true,
+    centeredSlides: false,
+    centerInsufficientSlides: true,
+    centeredSlidesBounds: true,
+    loop: true,
+    grabCursor: false,
+    simulateTouch: false,
+    autoplay: false,
+    navigation: {
+        nextEl: '.swiper-button-next.dress-hair',
+        prevEl: '.swiper-button-prev.dress-hair',
+    },
+    allowTouchMove: true,
+    preventClicks: true,
+});
+
+let swiperDressBackgroundList = new Swiper(".swiper.dress-background", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    autoHeight: true,
+    resizeObserver: true,
+    speed: 200,
+    allowSlideNext: true,
+    allowSlidePrev: true,
+    centeredSlides: false,
+    centerInsufficientSlides: true,
+    centeredSlidesBounds: true,
+    loop: true,
+    grabCursor: false,
+    simulateTouch: false,
+    autoplay: false,
+    navigation: {
+        nextEl: '.swiper-button-next.dress-background',
+        prevEl: '.swiper-button-prev.dress-background',
     },
     allowTouchMove: true,
     preventClicks: true,
