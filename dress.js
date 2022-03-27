314f36bed53a40a75f05d5e15cd5d40e4a0f0a92
gjlmotea <gjlmotea@gmail.com>
2022-03-27 13:04:20 +0800

    temp dress 2

--- dress.js
+++ dress.js
@@ -1,5 +1,6 @@
 <!-- 換裝區 swiper -->
-let swiperDressEye = new Swiper(".swiper.dress-eye", {
+let eyesIndex = 0;
+let swiperDressEyesList = new Swiper(".swiper.dress-eyes", {
     slidesPerView: 1,
     spaceBetween: 50,
     autoHeight: true,
@@ -8,14 +9,46 @@ let swiperDressEye = new Swiper(".swiper.dress-eye", {
     allowSlideNext: true,
     allowSlidePrev: true,
     centeredSlides: false,
-    centerInsufficientSlides: false,
+    centerInsufficientSlides: true,
+    centeredSlidesBounds: true,
     loop: true,
     grabCursor: false,
     simulateTouch: false,
     autoplay: false,
     navigation: {
-        nextEl: '.swiper-button-next.dress-eye',
-        prevEl: '.swiper-button-prev.dress-eye',
+        nextEl: '.swiper-button-next.dress-eyes',
+        prevEl: '.swiper-button-prev.dress-eyes',
+    },
+    allowTouchMove: true,
+    preventClicks: true,
+});
+
+
+// swiperDressEyesList.forEach(swiperDressEyes => {
+//     swiperDressEyes.on('slideChange', function (e) {
+//         eyesIndex = swiperDressEyes.realIndex;
+//         console.log(eyesIndex);
+//     });
+// })
+
+let swiperDressClothes = new Swiper(".swiper.dress-clothes", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    autoHeight: true,
+    resizeObserver: true,
+    speed: 500,
+    allowSlideNext: true,
+    allowSlidePrev: true,
+    centeredSlides: false,
+    centerInsufficientSlides: true,
+    centeredSlidesBounds: true,
+    loop: true,
+    grabCursor: false,
+    simulateTouch: false,
+    autoplay: false,
+    navigation: {
+        nextEl: '.swiper-button-next.dress-clothes',
+        prevEl: '.swiper-button-prev.dress-clothes',
     },
     allowTouchMove: true,
     preventClicks: true,
